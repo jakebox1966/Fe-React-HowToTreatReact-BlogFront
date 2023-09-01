a37546f168228653590c43119711bb94ee7f6651
jake <jakebox1966@gmail.com>
2023-09-01 16:06:58 +0900

    리액트를 다루는 기술(개정판) - blog - 헤더 컴포넌트 UI 작성 - 로그인 유지 및 로그아웃 관련 코드 작성

--- src/index.js
+++ src/index.js
@@ -11,12 +11,24 @@ import { Provider } from 'react-redux'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import { BrowserRouter } from 'react-router-dom'
 import createSagaMiddleware from '@redux-saga/core'
+import { check, tempSetUser } from './modules/user'
 
 const sagaMiddleware = createSagaMiddleware()
 
 const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(sagaMiddleware)))
 
+function loadUser() {
+    try {
+        const user = localStorage.getItem('user')
+        if (!user) return // 로그인 상태가 아니라면 아무것도 안 함
+        store.dispatch(tempSetUser(JSON.parse(user)))
+        store.dispatch(check())
+    } catch (e) {
+        console.log('localStorage is not working')
+    }
+}
 sagaMiddleware.run(rootSaga)
+loadUser()
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
--- src/pages/PostListPage.js
+++ src/pages/PostListPage.js
@@ -1,11 +1,12 @@
 import React from 'react'
-import Button from '../components/common/Button'
+import HeaderContainer from '../containers/common/HeaderContainer'
 
 function PostListPage() {
     return (
-        <div>
-            <Button>버튼</Button>
-        </div>
+        <>
+            <HeaderContainer />
+            <div>안녕</div>
+        </>
     )
 }
 
